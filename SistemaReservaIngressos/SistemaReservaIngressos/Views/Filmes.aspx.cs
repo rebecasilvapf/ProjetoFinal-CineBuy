dfba4eaa22dbbef567d943c9b24ae255b6402cbf
Rebeca Paulino <rebeca.paulino.df@gmail.com>
2024-09-24 00:01:05 -0300

    Alterações de comentarios

--- SistemaReservaIngressos/SistemaReservaIngressos/Views/Filmes.aspx.cs
+++ SistemaReservaIngressos/SistemaReservaIngressos/Views/Filmes.aspx.cs
@@ -38,7 +38,6 @@ namespace SistemaReservaIngressos
                         var jsonResponse = await response.Content.ReadAsStringAsync();
                         JArray filmes = JArray.Parse(jsonResponse);
 
-                        //Itera sobre os filmes e criar os controles
                         foreach (var filme in filmes)
                         {
                             //Cria o ImageButton dinamicamente
@@ -48,15 +47,15 @@ namespace SistemaReservaIngressos
                                 CssClass = "image-button-filmes",
                                 Height = 250,
                                 Width = 181,
-                                CommandArgument = filme["filmeId"].ToString()  // ID do filme (para identificação)
+                                CommandArgument = filme["filmeId"].ToString() 
                             };
-                            imgButton.Click += new ImageClickEventHandler(btnCompraClick); // Adiciona o evento de clique
+                            imgButton.Click += new ImageClickEventHandler(btnCompraClick);
 
                             //Limita o tamanho do titulo para não ultrapassar
                             string titulo = filme["titulo"].ToString();
                             if (titulo.Length > 20)
                             {
-                                titulo = titulo.Substring(0, 21) + "...";  // Corta o título e adiciona "..."
+                                titulo = titulo.Substring(0, 21) + "...";
                             }
 
                             // Cria o Label dinamicamente para exibir o título do filme
@@ -68,9 +67,9 @@ namespace SistemaReservaIngressos
 
                             // Cria um container (div) para organizar a exibição do botão e título
                             var divContainer = new Panel {};
-                            divContainer.Controls.Add(imgButton); // Adiciona o botão de imagem ao container
-                            divContainer.Controls.Add(new Literal { Text = "<br />" }); // Quebra de linha
-                            divContainer.Controls.Add(lblFilme);  // Adiciona o título ao container
+                            divContainer.Controls.Add(imgButton); 
+                            divContainer.Controls.Add(new Literal { Text = "<br />" }); 
+                            divContainer.Controls.Add(lblFilme);
 
                             // Adiciona o container ao controle principal na página (filmesContainer)
                             filmesContainer.Controls.Add(divContainer);
@@ -84,7 +83,7 @@ namespace SistemaReservaIngressos
                             Text = "Erro ao carregar os filmes.",
                             CssClass = "error-message" ,
                         };
-                        filmesContainer.Controls.Add(lblErro); // Adiciona a mensagem ao container principal
+                        filmesContainer.Controls.Add(lblErro);
                     }
                 }
             }
@@ -109,19 +108,18 @@ namespace SistemaReservaIngressos
 
                 //EndPoint da API referente a buscar o filme
                 string apiUrl = $"http://localhost:5109/api/Filme/BuscarFilme/{filmeId}";
-
-                //Instância do HttpClient para fazer requisições HTTP
+            
                 using (HttpClient client = new HttpClient()) 
                 {
-                    //Resposta do método da GET da API
+                    
                     HttpResponseMessage response = await client.GetAsync(apiUrl); 
 
-                    //Caso a resposta for sucesso 200
+                   
                     if (response.IsSuccessStatusCode) 
                     {
-                        Filme filme = await response.Content.ReadAsAsync<Filme>(); //Deserializa a resposta JSON para o objeto Filme
-                        string detalhesUrl = $"~/Views/ComprarFilme.aspx?id={filmeId}"; //Url da página de reserva/compra
-                        Response.Redirect(detalhesUrl); //Redirecionameto para a página
+                        Filme filme = await response.Content.ReadAsAsync<Filme>(); 
+                        string detalhesUrl = $"~/Views/ComprarFilme.aspx?id={filmeId}";
+                        Response.Redirect(detalhesUrl); 
                     }
                     else
                     {
