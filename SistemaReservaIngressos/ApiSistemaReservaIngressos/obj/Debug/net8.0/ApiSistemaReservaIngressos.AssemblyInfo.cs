0c52ccd1466bb4746a28b2dcad932e77e980cfc9
Rebeca Paulino <rebeca.paulino.df@gmail.com>
2024-09-24 21:27:58 -0300

    Adição do arquivo de documentação do projeto

--- SistemaReservaIngressos/ApiSistemaReservaIngressos/obj/Debug/net8.0/ApiSistemaReservaIngressos.AssemblyInfo.cs
+++ SistemaReservaIngressos/ApiSistemaReservaIngressos/obj/Debug/net8.0/ApiSistemaReservaIngressos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiSistemaReservaIngressos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9b4673e18730acb446272f4a463e9a504757ed2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfba4eaa22dbbef567d943c9b24ae255b6402cbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiSistemaReservaIngressos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiSistemaReservaIngressos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
